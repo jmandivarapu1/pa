23b4846a8150db05f4ba4b94e23cde9cbc5124b1
JayaKrishna Mandivarapu <jayanayana0@gmail.com>
2018-03-13 16:11:18 -0400

    Update pyramid_image.cu

--- pyramid_image.cu
+++ pyramid_image.cu
@@ -10,19 +10,11 @@
  */
 
 /**
- * Matrix multiplication: C = A * B.
+ * Pyramid of Images
  * Host code.
- *
- * This sample implements matrix multiplication as described in Chapter 3
- * of the programming guide.
- * It has been written for clarity of exposition to illustrate various CUDA
- * programming principles, not with the goal of providing the most
- * performant generic kernel for matrix multiplication.
- *
- * See also:
- * V. Volkov and J. Demmel, "Benchmarking GPUs to tune dense linear algebra,"
- * in Proc. 2008 ACM/IEEE Conf. on Supercomputing (SC '08),
- * Piscataway, NJ: IEEE Press, 2008, pp. Art. 31:1-11.
+ * for creating blurring then creating
+ * pyramid of images
+ * 
  */
 
 #include <stdio.h>
